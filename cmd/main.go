8b5ddaa10bd1679aeea6e7692ad5c7dbd5354e8d
Adam Bozanich <adam.boz@gmail.com>
2017-01-12 00:15:50 -0800

    second suite

--- cmd/main.go
+++ cmd/main.go
@@ -10,21 +10,21 @@ func WalkerDev() gestalt.Component {
 	g.AddChild(gestalt.SH("cleanup", "echo", "cleaning..."))
 	g.AddChild(
 		gestalt.SH("start", "while true; do echo .; sleep 1; done").WithBG(true)).
-		AddChild(g.AddChild(gestalt.SH("ping", "sleep", "3")))
+		AddChild(g.AddChild(gestalt.SH("ping", "sleep", "1")))
 	return g
 }
 
 func CreateFarm(name string) gestalt.Component {
 	g := gestalt.NewGroup("create-farm")
 	g.AddChild(gestalt.SH("create", "echo", "walker farms:create", name))
-	g.AddChild(gestalt.SH("test", "echo", "walker farms"))
+	g.AddChild(gestalt.SH("test", "sleep", "1"))
 	return g
 }
 
 func RemoveFarm(name string) gestalt.Component {
 	g := gestalt.NewGroup("remove-farm")
 	g.AddChild(gestalt.SH("remove", "echo", "walker farms:down", name))
-	g.AddChild(gestalt.SH("test", "echo", "walker farms"))
+	g.AddChild(gestalt.SH("test", "sleep", "1"))
 	return g
 }
 
@@ -37,9 +37,23 @@ func FarmSuite() gestalt.Component {
 	return s
 }
 
+func LeaderSuite() gestalt.Component {
+	s := gestalt.NewSuite("leaders")
+	s.AddChild(WalkerDev())
+
+	s.AddChild(gestalt.SH("create", "echo", "waker leaders:up l1")).
+		AddChild(gestalt.SH("test", "sleep", "1"))
+
+	s.AddChild(gestalt.SH("create", "echo", "waker leaders:down l1")).
+		AddChild(gestalt.SH("test", "sleep", "1"))
+	return s
+}
+
 func main() {
 	logrus.SetLevel(logrus.DebugLevel)
-	s := FarmSuite()
+	s := gestalt.NewSuite("walker")
+	s.AddChild(FarmSuite())
+	s.AddChild(LeaderSuite())
 	if err := gestalt.Run(s); err != nil {
 		logrus.Errorf("Error running: %v", err)
 	}
