7171de9af7b9ac28af2e7862e0c39b134e81b628
Adam Bozanich <adam.boz@gmail.com>
2017-01-16 17:53:57 -0800

    checkpt

--- vars/meta.go
+++ vars/meta.go
@@ -3,24 +3,41 @@ package vars
 type Meta interface {
 	Require(...string) Meta
 	Export(...string) Meta
+	Requires() []string
+	Exports() []string
 	Merge(Meta) Meta
+}
+
+type meta struct {
+	exports  []string
+	requires []string
 }
 
 func NewMeta() Meta {
 	return &meta{}
 }
 
-type meta struct {
+func (m *meta) Requires() []string {
+	return m.requires
+}
+
+func (m *meta) Exports() []string {
+	return m.exports
 }
 
 func (m *meta) Require(keys ...string) Meta {
+	m.requires = append(m.requires, keys...)
 	return m
 }
 
 func (m *meta) Export(keys ...string) Meta {
+	m.exports = append(m.exports, keys...)
 	return m
 }
 
 func (m *meta) Merge(other Meta) Meta {
-	return m
+	return &meta{
+		requires: append(m.requires, other.Requires()...),
+		exports:  append(m.exports, other.Requires()...),
+	}
 }
